fe4391e9b5debf3feeaf47a0fdea8269bc4f8128
Phat Tran <phat.tran.developer@gmail.com>
2024-08-06 19:08:57 +1000

    Update model config

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     train_last_layer_sinkhorn({
         "lr": 10e-6,
         "save_dir": "result",
-        "save_name": "result_last_layer_sinkhorn_5_iter.npy",
-        "output_model_prefix": "weights/model_last_layer_sinkhorn_iter_5.pth",
+        "save_name": "result_last_layer_sinkhorn_7_iter.npy",
+        "output_model_prefix": "weights/model_last_layer_sinkhorn_iter_7.pth",
         "epochs": 20,
         "hidden_size": 256,
     })
