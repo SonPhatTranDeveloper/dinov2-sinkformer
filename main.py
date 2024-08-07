54603c34848d8323887e1c8407fec21533153671
Phat Tran <phat.tran.developer@gmail.com>
2024-08-07 16:07:50 +1000

    Update iterations

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         "data": "data/imagenette",
         "lr": 10e-6,
         "save_dir": "result/imagenette",
-        "save_name": "result_last_layer_sinkhorn_3_iter.npy",
-        "output_model_prefix": "weights/imagenette/model_last_layer_sinkhorn_3_iter.pth",
+        "save_name": "result_last_layer_sinkhorn_5_iter.npy",
+        "output_model_prefix": "weights/imagenette/model_last_layer_sinkhorn_5_iter.pth",
         "epochs": 20,
         "hidden_size": 256,
     })
