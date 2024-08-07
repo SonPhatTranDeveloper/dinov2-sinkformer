37f237b1f8657c9affffd645a1ff4220c737b4c7
Phat Tran <phat.tran.developer@gmail.com>
2024-08-07 16:51:12 +1000

    Update model

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from trainer import train_last_layer_sinkhorn
+from trainer import train_baseline
 
 
 if __name__ == "__main__":
     # Train sinkhorn
-    train_last_layer_sinkhorn({
+    train_baseline({
         "data": "data/imagenette",
         "lr": 10e-6,
         "save_dir": "result/imagenette",
-        "save_name": "result_last_layer_sinkhorn_5_iter.npy",
-        "output_model_prefix": "weights/imagenette/model_last_layer_sinkhorn_5_iter.pth",
+        "save_name": "result_softmax.npy",
+        "output_model_prefix": "weights/imagenette/model_softmax.pth",
         "epochs": 20,
         "hidden_size": 256,
     })
