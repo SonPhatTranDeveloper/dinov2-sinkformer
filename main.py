c93517ee038d89583db947bdbc6f0920b6e081ac
Phat Tran <phat.tran.developer@gmail.com>
2024-08-08 15:38:33 +1000

    Update model

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         "data": "data/imagenette",
         "lr": 10e-6,
         "save_dir": "result/imagenette",
-        "save_name": "result_full_sinkhorn_5_iter.npy",
-        "output_model_prefix": "weights/imagenette/model_full_sinkhorn_5_iter.pth",
+        "save_name": "result_full_sinkhorn_7_iter.npy",
+        "output_model_prefix": "weights/imagenette/model_full_sinkhorn_7_iter.pth",
         "epochs": 20,
         "hidden_size": 256,
     })
