e33e9c8546afe418bb67c9dc9f922b31bc75208b
Phat Tran <phat.tran.developer@gmail.com>
2024-08-11 17:55:42 +1000

    Update model

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from trainer import train_last_layer_sinkhorn
+from trainer import train_baseline
+import torch
+import random
+import numpy as np
+
+# Set random seeds
+torch.manual_seed(1120)
+random.seed(1120)
+np.random.seed(1120)
 
 
 if __name__ == "__main__":
     # Train sinkhorn with different weights
-    train_last_layer_sinkhorn({
+    train_baseline({
         "data": "data/cub200",
         "lr": 10e-6,
         "save_dir": "result/cub200",
-        "save_name": "result_last_layer_sinkhorn_5_iter.npy",
-        "output_model_prefix": "weights/cub200/model_last_layer_sinkhorn_5_iter.pth",
-        "epochs": 20,
+        "save_name": "result_baseline.npy",
+        "output_model_prefix": "weights/cub200/model_baseline.pth",
+        "epochs": 30,
         "hidden_size": 256,
     })
