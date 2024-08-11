5a8334bfc7c3f00da77cf316c6b4b0961765d4ad
Phat Tran <phat.tran.developer@gmail.com>
2024-08-11 15:18:55 +1000

    Update model for Image Wang

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from trainer import train_last_layer_weighted_fixed
+from trainer import train_last_layer_sinkhorn
 
 
 if __name__ == "__main__":
     # Train sinkhorn with different weights
-    train_last_layer_weighted_fixed({
-        "data": "data/imagewoof",
+    train_last_layer_sinkhorn({
+        "data": "data/imagewang",
         "lr": 10e-6,
-        "save_dir": "result/imagewoof",
-        "save_name": "result_last_layer_weighted_0_1_5_iter.npy",
-        "output_model_prefix": "weights/imagenette/model_last_layer_weighted_0_1_5_iter.pth",
+        "save_dir": "result/imagewang",
+        "save_name": "result_last_layer_3_iter.npy",
+        "output_model_prefix": "weights/imagenette/model_last_layer_3_iter.pth",
         "epochs": 20,
         "hidden_size": 256,
     })
