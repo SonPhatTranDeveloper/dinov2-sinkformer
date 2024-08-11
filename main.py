b3c75cbcd1cf4ea02b225215dc5be73923121762
Phat Tran <phat.tran.developer@gmail.com>
2024-08-11 16:17:26 +1000

    Update model

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from trainer import train_last_layer_sinkhorn
+from trainer import train_baseline
 
 
 if __name__ == "__main__":
     # Train sinkhorn with different weights
-    train_last_layer_sinkhorn({
+    train_baseline({
         "data": "data/cub200",
         "lr": 10e-6,
         "save_dir": "result/cub200",
-        "save_name": "result_last_layer_3_iter.npy",
-        "output_model_prefix": "weights/cub200/model_last_layer_3_iter.pth",
+        "save_name": "result_baseline.npy",
+        "output_model_prefix": "weights/cub200/model_baseline.pth",
         "epochs": 20,
         "hidden_size": 256,
     })
