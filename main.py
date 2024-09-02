5b77d4e0fc3e8430c5de26457f52c29ef52f274c
Phat Tran <phat.tran.developer@gmail.com>
2024-09-02 10:33:33 +1000

    Update model

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         "data": "data/cub200",
         "lr": 10e-6,
         "save_dir": "result/cub200",
-        "save_name": "last_layer_sinkhorn_5.npy",
-        "output_model_prefix": "weights/cub200/last_layer_sinkhorn_5.pth",
+        "save_name": "last_layer_sinkhorn_7.npy",
+        "output_model_prefix": "weights/cub200/last_layer_sinkhorn_7.pth",
         "epochs": 30,
         "hidden_size": 256,
         "k": 5
